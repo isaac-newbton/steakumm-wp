cb4b1cc57149c8472beced7cbc0375621c3e7758
Micah Gentzel <mgentzel@allebach.com>
2021-03-15 10:56:50 -0400

    add support for still images to homepage slider

--- page-home-page.php
+++ page-home-page.php
@@ -24,19 +24,36 @@ get_header();
         ?>
         <div id="homepage-videos" class="owl-carousel owl-theme">
             <?php while (have_rows('top_slider')) : the_row(); ?>
-            <div class="item">
-                <div class="video-wrapper">
-                    <?php if (get_sub_field('video_link')) : ?><a href=""><?php endif;?>
-                    <video class="desktop-only" src="<?php the_sub_field('video_url'); ?>" muted playsinline></video>
-                    <?php if (get_sub_field('video_link')) : ?></a><?php endif;?>
+            <?php
+                $video_url = get_sub_field('video_url');
+                $video_url_responsive = get_sub_field('video_url_responsive');
+                $video_link = get_sub_field('video_link');
+                $image_url = get_sub_field('still_image_url');
+                $image_link = get_sub_field('image_link');
+            ?>
+            <?php if($video_url && ''!==$video_url): ?>
+                <div class="item">
+                    <div class="video-wrapper">
+                        <?php if (get_sub_field('video_link')) : ?><a href=""><?php endif;?>
+                        <video class="desktop-only" src="<?php the_sub_field('video_url'); ?>" muted playsinline></video>
+                        <?php if (get_sub_field('video_link')) : ?></a><?php endif;?>
 
-                    <?php if (get_sub_field('video_url_responsive')) : ?>
-                    <?php if (get_sub_field('video_link')) : ?><a href="<?php the_sub_field('video_link'); ?>"><?php endif;?>
-                    <video class="mobile-only" src="<?php the_sub_field('video_url_responsive'); ?>" muted playsinline></video>
-                    <?php if (get_sub_field('video_link')) : ?></a><?php endif;?>
-                    <?php endif; ?>
+                        <?php if (get_sub_field('video_url_responsive')) : ?>
+                        <?php if (get_sub_field('video_link')) : ?><a href="<?php the_sub_field('video_link'); ?>"><?php endif;?>
+                        <video class="mobile-only" src="<?php the_sub_field('video_url_responsive'); ?>" muted playsinline></video>
+                        <?php if (get_sub_field('video_link')) : ?></a><?php endif;?>
+                        <?php endif; ?>
+                    </div>
                 </div>
-            </div>
+            <?php elseif($image_url && ''!==$image_url): ?>
+                <div class="item">
+                    <div class="image-wrapper">
+                        <?php if($image_link): ?><a href="<?=$image_link?>"><?php endif; ?>
+                            <img src="<?=$image_url?>" alt="">
+                        <?php if($image_link): ?></a><?php endif; ?>
+                    </div>
+                </div>
+            <?php endif; ?>
             <?php endwhile; ?>
         </div>
         <?php
