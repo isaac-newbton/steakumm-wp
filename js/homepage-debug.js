a7b666bfe4c60c4a13466e8e628d7c2e1444ddb8
Micah Gentzel <mgentzel@allebach.com>
2021-03-16 12:52:10 -0400

    homepage carousel loop

--- js/homepage-debug.js
+++ js/homepage-debug.js
@@ -52,7 +52,7 @@ document.addEventListener('DOMContentLoaded', () => {
         autoHeight: true,
         navSpeed: 600,
         autoplay: true,
-        autoplayTimeout: 3000,
+        autoplayTimeout: 6000,
         autoplayHoverPause: true,
         onInitialized: initVids,
         onTranslated: playVid,
